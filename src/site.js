f918a48c974e21151e71709afde45938e13dfa34
fialo4ka <tona16@inbox.lv>
2020-12-25 00:14:06 +0100

    change design

--- src/site.js
+++ src/site.js
@@ -1,47 +1,15 @@
-if (window.addEventListener) {
-    let passiveSupported = false;
-        try {
-            const options = {
-                get passive() { 
-                    passiveSupported = true;
-                    return false;
-                }
-            };
-            // IE9, Chrome, Safari, Opera
-            window.addEventListener("mousewheel", scrollHorizontally, options);
-            // Firefox
-            window.addEventListener("DOMMouseScroll", scrollHorizontally, options);
-        } 
-        catch(err) {
-            passiveSupported = false;
-        }
-    }
-   
+// Toggle between showing and hiding the sidebar when clicking the menu icon
+var mySidebar = document.getElementById("mySidebar");
 
-
-
-function scrollHorizontally(e) {
-    let hjhg = !isInViewport(document.getElementById("scrollAkril"));
-    if(hjhg)
-    {
-        console.log(hjhg);
-        return;
-    } 
-    e = window.event || e;
-    var delta = Math.max(-1, Math.min(1, (e.wheelDelta || -e.detail)));
-    var scrollSpeed = 60; // Janky jank <<<<<<<<<<<<<<
-    document.documentElement.scrollLeft -= (delta * scrollSpeed);
-    document.body.scrollLeft -= (delta * scrollSpeed);
-    e.preventDefault();
+function w3_open() {
+  if (mySidebar.style.display === 'block') {
+    mySidebar.style.display = 'none';
+  } else {
+    mySidebar.style.display = 'block';
   }
+}
 
-function isInViewport(element) {
-    const rect = element.getBoundingClientRect();
-    console.log(rect.top+ "  " + window.innerHeight);
-
-    return (
-        rect.top >= 0 && 
-        rect.top <= window.innerHeight
-        
-    );
+// Close the sidebar with the close button
+function w3_close() {
+    mySidebar.style.display = "none";
 }
