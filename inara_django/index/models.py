746f6b8b78f55d1670e765188faab8b0e526d548
fialo4ka <tona16@inbox.lv>
2021-01-13 12:41:28 +0100

    add models to db

--- inara_django/index/models.py
+++ inara_django/index/models.py
@@ -1,3 +1,20 @@
 from django.db import models
 
 # Create your models here.
+class ArtWork(models.Model):
+    id = models.AutoField(primary_key=True)
+    path = models.CharField(max_length=100)
+    ART_TYPE = (
+        ('P', 'PAINTINGS'),
+        ('W', 'WATERCOLORS'),
+        ('G', 'GRAPHICS'),
+        ('D', 'DESIGN'),
+    )
+    STATE = (
+        ('S', 'In private collection'),
+        ('Fr', 'For Sale'),
+    )
+    price = models.DecimalField(decimal_places=2, max_digits=10, null=True)
+    
+    def save(self, *args, **kwargs):
+        super().save(*args, **kwargs)  # Call the "real" save() method.
