2cee95c8d4c52bbeff46032350bffed73007434a
fialo4ka <tona16@inbox.lv>
2021-02-07 15:51:08 +0100

    add more, templates

--- index/urls.py
+++ index/urls.py
@@ -3,4 +3,5 @@ from index import views
 
 urlpatterns = [
     path("", views.home, name="home"),
+    path("more/<int:art_type>/", views.more_images, name="more"),
 ]
--- index/views.py
+++ index/views.py
@@ -2,22 +2,61 @@ import re
 from datetime import datetime
 from django.http import HttpResponse
 from django.shortcuts import render
-from .models import ArtWork
+from .models import ArtWork, ArtType
 import operator
 
 def home(request):
-    paint_list1 = ArtWork.objects.all().filter(art_type=1, column_number=1).order_by('sort')
-    paint_list2 = ArtWork.objects.all().filter(art_type=1, column_number=2).order_by('sort')
-    paint_list3 = ArtWork.objects.all().filter(art_type=1, column_number=3).order_by('sort')
+    tempList = ArtWork.objects.all().filter(art_type=1, column_number=1, art_status=2).order_by('sort')
+    if tempList.count() >= 3:
+        paint_list1 = tempList
+    else:
+        paint_list1 = ArtWork.objects.all().filter(art_type=1, column_number=1).order_by('sort')[:5]
+    tempList = ArtWork.objects.all().filter(art_type=1, column_number=2, art_status=2).order_by('sort')
+    if tempList.count() >= 3:
+        paint_list2 = tempList
+    else:
+        paint_list2 = ArtWork.objects.all().filter(art_type=1, column_number=2).order_by('sort')[:5]
+    tempList = ArtWork.objects.all().filter(art_type=1, column_number=3, art_status=2).order_by('sort')
+    if tempList.count() >= 3:
+        paint_list3= tempList
+    else:
+        paint_list3 = ArtWork.objects.all().filter(art_type=1, column_number=3).order_by('sort')[:5]
+
+
+    tempList = ArtWork.objects.all().filter(art_type=2, column_number=1, art_status=2).order_by('sort')
+    if tempList.count() >= 3:
+        water_list1= tempList
+    else:
+        water_list1 = ArtWork.objects.all().filter(art_type=2, column_number=1).order_by('sort')
+    tempList = ArtWork.objects.all().filter(art_type=2, column_number=2, art_status=2).order_by('sort')
+    if tempList.count() >= 3:
+        water_list2= tempList
+    else:
+        water_list2 = ArtWork.objects.all().filter(art_type=2, column_number=2).order_by('sort')
+    tempList = ArtWork.objects.all().filter(art_type=2, column_number=3, art_status=2).order_by('sort')
+    if tempList.count() >= 3:
+        water_list3= tempList
+    else:
+        water_list3 = ArtWork.objects.all().filter(art_type=2, column_number=3).order_by('sort')
+
+
     
-    water_list1 = ArtWork.objects.all().filter(art_type=2, column_number=1).order_by('sort')
-    water_list2 = ArtWork.objects.all().filter(art_type=2, column_number=2).order_by('sort')
-    water_list3 = ArtWork.objects.all().filter(art_type=2, column_number=3).order_by('sort')
-    
-    graph_list1 = ArtWork.objects.all().filter(art_type=3, column_number=1).order_by('sort')
-    graph_list2 = ArtWork.objects.all().filter(art_type=3, column_number=2).order_by('sort')
-    graph_list3 = ArtWork.objects.all().filter(art_type=3, column_number=3).order_by('sort')
-    
+    tempList = ArtWork.objects.all().filter(art_type=3, column_number=1, art_status=2).order_by('sort')
+    if tempList.count() >= 3:
+        graph_list1= tempList
+    else:
+        graph_list1 = ArtWork.objects.all().filter(art_type=3, column_number=1).order_by('sort')
+    tempList = ArtWork.objects.all().filter(art_type=3, column_number=2, art_status=2).order_by('sort')
+    if tempList.count() >= 3:
+        graph_list2= tempList
+    else:
+        graph_list2 = ArtWork.objects.all().filter(art_type=3, column_number=2).order_by('sort')    
+    tempList = ArtWork.objects.all().filter(art_type=3, column_number=3, art_status=2).order_by('sort')
+    if tempList.count() >= 3:
+        graph_list3= tempList
+    else:
+        graph_list3 = ArtWork.objects.all().filter(art_type=3, column_number=3).order_by('sort')   
+
     des_list1 = ArtWork.objects.all().filter(art_type=4, column_number=1).order_by('sort')
     des_list2 = ArtWork.objects.all().filter(art_type=4, column_number=2).order_by('sort')
     des_list3 = ArtWork.objects.all().filter(art_type=4, column_number=3).order_by('sort')
@@ -29,7 +68,7 @@ def home(request):
 
     return render(
         request,
-        'index/index_there.html',
+        'index/index_home.html',
         context = {
             'paint_list1': paint_list1,
             'paint_list2': paint_list2,
@@ -46,5 +85,24 @@ def home(request):
             'print_list1': print_list1,
             'print_list2': print_list2,
             'print_list3': print_list3
+        }
+    )
+
+
+def more_images(request, art_type):
+    list1 = ArtWork.objects.all().filter(art_type=art_type, column_number=1).order_by('sort') 
+    list2 = ArtWork.objects.all().filter(art_type=art_type, column_number=2).order_by('sort')
+    list3 = ArtWork.objects.all().filter(art_type=art_type, column_number=3).order_by('sort')
+    
+    artType = ArtType.objects.get(id=art_type)
+    
+    return render(
+        request,
+        'index/more_image.html',
+        context = {
+            'list1': list1,
+            'list2': list2,
+            'list3': list3,
+            'artType': artType
         }
     )
